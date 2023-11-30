6856530ebcbaf67fea1276dad9470a35ae7eeacf
AndriiLendiel <lendiel9333@gmail.com>
2023-11-30 15:18:55 +0200

    done3

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React from "react";
 import { Title } from "utils/utils";
 import {FormWrapper,MainForm,FormLabel,FormInput, FormButton, ErrorText} from "./ContactForm.styled";
 
